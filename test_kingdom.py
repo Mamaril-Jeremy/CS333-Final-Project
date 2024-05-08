8c955f1432aceac61206db38d19f4645f9dc0be6
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2024-05-07 18:43:04 -0700

    finished integration tests

--- test_kingdom.py
+++ test_kingdom.py
@@ -13,11 +13,6 @@ class TestKingdom(unittest.TestCase):
         self.kingdom.addCustomer(customer)
         self.assertIn(customer, self.kingdom.getCustomers())
 
-    def test_removeFromInventory(self):
-        dog_name = "bulldog"
-        self.kingdom.removeFromInventory(dog_name)
-        self.assertNotIn(dog_name, self.kingdom.getInventory())
-
     def test_completeTransaction(self):
         customer = Customer("Jeremy")
         dog_name = "husky"
@@ -42,6 +37,41 @@ class TestKingdom(unittest.TestCase):
         self.kingdom.addCustomer(customer1)
         self.kingdom.addCustomer(customer2)
         self.assertEqual(len(self.kingdom.getCustomers()), 2)
+
+    def test_getCustomerNamePerTransaction(self):
+        customer = Customer("Mike")
+        new_transaction = Transaction(customer, "shihtzu", 100)
+        self.assertEqual(new_transaction.getCustomer().getName(), "Mike")
+
+    def test_removeFromInventoryExistingDog(self):
+        kingdom = Kingdom()
+        dog_name = "poodle"
+        kingdom.removeFromInventory(dog_name)
+        self.assertNotIn(dog_name, kingdom.getInventory())
+
+    def test_removeFromInventoryNonExistingDog(self):
+        kingdom = Kingdom()
+        non_existing_dog = "unicorn"
+        result = kingdom.removeFromInventory(non_existing_dog)
+        self.assertFalse(result)
+
+    def test_getCustomerHistoryPerTransaction(self):
+        kingdom = Kingdom()
+        customer = Customer("Mike")
+        new_transaction = Transaction(customer, "shihtzu", 100)
+        kingdom.addCustomer(customer)
+        customer.addToHistory(new_transaction.getDogName())
+        kingdom.completeTransaction(new_transaction)
+        self.assertIn("shihtzu", kingdom.getCustomers()[0].getHistory())
+
+    def test_getTransactionUponAddingCustomer(self):
+        kingdom = Kingdom()
+        customer = Customer("Mike")
+        new_transaction = Transaction(customer, "shihtzu", 100)
+        kingdom.completeTransaction(new_transaction)
+        self.assertEqual(kingdom.getHistory()[0].getCustomer().getName(), "Mike")
+        self.assertEqual(kingdom.getHistory()[0].getDogName(), "shihtzu")
+        self.assertEqual(kingdom.getHistory()[0].getPrice(), 100)
 
 if __name__ == '__main__':
     unittest.main()
